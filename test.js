d63847036eae10b93e510a46257252d2f7cf12fe
puravchauhan142519 <purav.chauhan142519@marwadiuniversity.ac.in>
2026-09-22 18:49:33 +0530

    Add division operation

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ const { calculate } = require('./app.js');
 assert.equal(calculate(2, 'add', 3), 5);
 assert.equal(calculate(8, 'subtract', 3), 5);
 assert.equal(calculate(4, 'multiply', 5), 20);
-
+assert.equal(calculate(8, 'divide', 2), 4);
 console.log('All JavaScript tests passed.');
